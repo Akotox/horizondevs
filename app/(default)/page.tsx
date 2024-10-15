f90ac2534e072bb58e8f6cb2d1ce10a19cd58cc9
Akotox <akotoxmpimbo@gmail.com>
2024-10-15 17:33:16 -0400

    metadata

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -9,7 +9,7 @@ export const metadata = {
     siteName: "Horizon Developers",
     images: [
       {
-        url: "hhttps://firebasestorage.googleapis.com/v0/b/electro-523c1.appspot.com/o/features.png?alt=media&token=a456925b-fa96-4996-9eef-fb554800eb17",
+        url: "https://firebasestorage.googleapis.com/v0/b/electro-523c1.appspot.com/o/features.png?alt=media&token=a456925b-fa96-4996-9eef-fb554800eb17",
         width: 800,
         height: 600,
         alt: "Description of the image",
@@ -20,7 +20,7 @@ export const metadata = {
     card: 'summary_large_image',
     site: '@YourTwitterHandle',
     title: "Horizon Developers",
-    description: "Page description",
+    description: "Horizon Developers: Specializing in full-stack web development and custom software solutions, we create efficient, user-friendly web applications tailored to your needs. Partner with us to turn your ideas into reality!",
     image: "https://firebasestorage.googleapis.com/v0/b/electro-523c1.appspot.com/o/features.png?alt=media&token=a456925b-fa96-4996-9eef-fb554800eb17",
   },
   canonical: "https://horizondevelopers.co.za",
