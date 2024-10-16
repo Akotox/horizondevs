9234e9985a9f1a92b4edca647e4877358b8198c0
Akotox <akotoxmpimbo@gmail.com>
2024-10-16 04:22:41 -0400

    coming soon

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -3,6 +3,7 @@
 import Logo from "./logo";
 import Image from "next/image";
 import FooterIllustration from "@/public/images/footer-illustration.svg";
+import Link from "next/link";
 
 export default function Footer() {
   return (
@@ -27,45 +28,45 @@ export default function Footer() {
             <h3 className="text-sm font-medium text-gray-200">Product</h3>
             <ul className="space-y-2 text-sm">
               <li>
-                <a
+                <Link
                   className="text-indigo-200/65 transition hover:text-indigo-500"
-                  href="#0"
+                  href="/underconstruction"
                 >
                   Features
-                </a>
+                </Link>
               </li>
               <li>
-                <a
+              <Link
                   className="text-indigo-200/65 transition hover:text-indigo-500"
-                  href="#0"
+                  href="/underconstruction"
                 >
                   Integrations
-                </a>
+                </Link>
               </li>
               <li>
-                <a
+              <Link
                   className="text-indigo-200/65 transition hover:text-indigo-500"
-                  href="#0"
+                  href="/underconstruction"
                 >
                   Pricing &amp; Plans
-                </a>
+                </Link>
               </li>
               
               <li>
-                <a
+              <Link
                   className="text-indigo-200/65 transition hover:text-indigo-500"
-                  href="#0"
+                  href="/underconstruction"
                 >
                   Our method
-                </a>
+                </Link>
               </li>
               <li>
-                <a
+              <Link
                   className="text-indigo-200/65 transition hover:text-indigo-500"
-                  href="#0"
+                  href="/underconstruction"
                 >
                   User policy
-                </a>
+                </Link>
               </li>
             </ul>
           </div>
@@ -74,37 +75,37 @@ export default function Footer() {
             <h3 className="text-sm font-medium text-gray-200">Company</h3>
             <ul className="space-y-2 text-sm">
               <li>
-                <a
+              <Link
                   className="text-indigo-200/65 transition hover:text-indigo-500"
-                  href="#0"
+                  href="/underconstruction"
                 >
                   About us
-                </a>
+                </Link>
               </li>
               
               <li>
-                <a
+                <Link
                   className="text-indigo-200/65 transition hover:text-indigo-500"
-                  href="#0"
+                  href="/blogs"
                 >
                   Blog
-                </a>
+                </Link>
               </li>
               <li>
-                <a
+              <Link
                   className="text-indigo-200/65 transition hover:text-indigo-500"
-                  href="#0"
+                  href="/underconstruction"
                 >
                   Careers
-                </a>
+                </Link>
               </li>
               <li>
-                <a
+              <Link
                   className="text-indigo-200/65 transition hover:text-indigo-500"
-                  href="#0"
+                  href="/underconstruction"
                 >
                   Financial statements
-                </a>
+                </Link>
               </li>
             </ul>
           </div>
@@ -113,28 +114,28 @@ export default function Footer() {
             <h3 className="text-sm font-medium text-gray-200">Resources</h3>
             <ul className="space-y-2 text-sm">
               <li>
-                <a
+              <Link
                   className="text-indigo-200/65 transition hover:text-indigo-500"
-                  href="#0"
+                  href="/underconstruction"
                 >
                   Community
-                </a>
+                </Link>
               </li>
               <li>
-                <a
+                <Link
                   className="text-indigo-200/65 transition hover:text-indigo-500"
-                  href="#0"
+                  href="/underconstruction"
                 >
                   Terms of service
-                </a>
+                </Link>
               </li>
               <li>
-                <a
+              <Link
                   className="text-indigo-200/65 transition hover:text-indigo-500"
-                  href="#0"
+                  href="/underconstruction"
                 >
                   Report a vulnerability
-                </a>
+                </Link>
               </li>
             </ul>
           </div>
@@ -145,28 +146,28 @@ export default function Footer() {
             </h3>
             <ul className="space-y-2 text-sm">
               <li>
-                <a
+              <Link
                   className="text-indigo-200/65 transition hover:text-indigo-500"
-                  href="#0"
+                  href="/underconstruction"
                 >
                   Templates
-                </a>
+                </Link>
               </li>
               <li>
-                <a
+              <Link
                   className="text-indigo-200/65 transition hover:text-indigo-500"
-                  href="#0"
+                  href="/underconstruction"
                 >
                   Tutorials
-                </a>
+                </Link>
               </li>
               <li>
-                <a
+              <Link
                   className="text-indigo-200/65 transition hover:text-indigo-500"
-                  href="#0"
+                  href="/underconstruction"
                 >
                   Knowledge base
-                </a>
+                </Link>
               </li>
               <li>
                 <a
@@ -188,53 +189,48 @@ export default function Footer() {
                 © Horizon Developers
                 <span className="text-gray-700"> 2024 </span>
               </p>
-              <ul className="inline-flex gap-1">
-                <li>
-                  <a
-                    className="flex items-center justify-center text-indigo-500 transition hover:text-indigo-400"
-                    href="#0"
-                    aria-label="Twitter"
-                  >
-                    <svg
-                      className="h-8 w-8 fill-current"
-                      viewBox="0 0 32 32"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path d="m13.063 9 3.495 4.475L20.601 9h2.454l-5.359 5.931L24 23h-4.938l-3.866-4.893L10.771 23H8.316l5.735-6.342L8 9h5.063Zm-.74 1.347h-1.457l8.875 11.232h1.36l-8.778-11.232Z" />
-                    </svg>
-                  </a>
-                </li>
-                <li>
-                  <a
-                    className="flex items-center justify-center text-indigo-500 transition hover:text-indigo-400"
-                    href="#0"
-                    aria-label="Medium"
-                  >
-                    <svg
-                      className="h-8 w-8 fill-current"
-                      viewBox="0 0 32 32"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path d="M23 8H9a1 1 0 0 0-1 1v14a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1V9a1 1 0 0 0-1-1Zm-1.708 3.791-.858.823a.251.251 0 0 0-.1.241V18.9a.251.251 0 0 0 .1.241l.838.823v.181h-4.215v-.181l.868-.843c.085-.085.085-.11.085-.241v-4.887l-2.41 6.131h-.329l-2.81-6.13V18.1a.567.567 0 0 0 .156.472l1.129 1.37v.181h-3.2v-.181l1.129-1.37a.547.547 0 0 0 .146-.472v-4.749a.416.416 0 0 0-.138-.351l-1-1.209v-.181H13.8l2.4 5.283 2.122-5.283h2.971l-.001.181Z" />
-                    </svg>
-                  </a>
-                </li>
-                <li>
-                  <a
-                    className="flex items-center justify-center text-indigo-500 transition hover:text-indigo-400"
-                    href="https://github.com/Akotox"
-                    aria-label="Github"
-                  >
-                    <svg
-                      className="h-8 w-8 fill-current"
-                      viewBox="0 0 32 32"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path d="M16 8.2c-4.4 0-8 3.6-8 8 0 3.5 2.3 6.5 5.5 7.6.4.1.5-.2.5-.4V22c-2.2.5-2.7-1-2.7-1-.4-.9-.9-1.2-.9-1.2-.7-.5.1-.5.1-.5.8.1 1.2.8 1.2.8.7 1.3 1.9.9 2.3.7.1-.5.3-.9.5-1.1-1.8-.2-3.6-.9-3.6-4 0-.9.3-1.6.8-2.1-.1-.2-.4-1 .1-2.1 0 0 .7-.2 2.2.8.6-.2 1.3-.3 2-.3s1.4.1 2 .3c1.5-1 2.2-.8 2.2-.8.4 1.1.2 1.9.1 2.1.5.6.8 1.3.8 2.1 0 3.1-1.9 3.7-3.7 3.9.3.4.6.9.6 1.6v2.2c0 .2.1.5.6.4 3.2-1.1 5.5-4.1 5.5-7.6-.1-4.4-3.7-8-8.1-8z" />
-                    </svg>
-                  </a>
-                </li>
-              </ul>
+              <ul className="inline-flex gap-3">
+  <li>
+    <a href="https://example.com/link1" className="[&>svg]:h-7 [&>svg]:w-7 [&>svg]:fill-[#7289da]">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512">
+        <path
+          d="M524.5 69.8a1.5 1.5 0 0 0 -.8-.7A485.1 485.1 0 0 0 404.1 32a1.8 1.8 0 0 0 -1.9 .9 337.5 337.5 0 0 0 -14.9 30.6 447.8 447.8 0 0 0 -134.4 0 309.5 309.5 0 0 0 -15.1-30.6 1.9 1.9 0 0 0 -1.9-.9A483.7 483.7 0 0 0 116.1 69.1a1.7 1.7 0 0 0 -.8 .7C39.1 183.7 18.2 294.7 28.4 404.4a2 2 0 0 0 .8 1.4A487.7 487.7 0 0 0 176 479.9a1.9 1.9 0 0 0 2.1-.7A348.2 348.2 0 0 0 208.1 430.4a1.9 1.9 0 0 0 -1-2.6 321.2 321.2 0 0 1 -45.9-21.9 1.9 1.9 0 0 1 -.2-3.1c3.1-2.3 6.2-4.7 9.1-7.1a1.8 1.8 0 0 1 1.9-.3c96.2 43.9 200.4 43.9 295.5 0a1.8 1.8 0 0 1 1.9 .2c2.9 2.4 6 4.9 9.1 7.2a1.9 1.9 0 0 1 -.2 3.1 301.4 301.4 0 0 1 -45.9 21.8 1.9 1.9 0 0 0 -1 2.6 391.1 391.1 0 0 0 30 48.8 1.9 1.9 0 0 0 2.1 .7A486 486 0 0 0 610.7 405.7a1.9 1.9 0 0 0 .8-1.4C623.7 277.6 590.9 167.5 524.5 69.8zM222.5 337.6c-29 0-52.8-26.6-52.8-59.2S193.1 219.1 222.5 219.1c29.7 0 53.3 26.8 52.8 59.2C275.3 311 251.9 337.6 222.5 337.6zm195.4 0c-29 0-52.8-26.6-52.8-59.2S388.4 219.1 417.9 219.1c29.7 0 53.3 26.8 52.8 59.2C470.7 311 447.5 337.6 417.9 337.6z" />
+      </svg>
+    </a>
+  </li>
+  <li>
+    <a href="https://example.com/link2" className="[&>svg]:h-7 [&>svg]:w-7 [&>svg]:fill-[#1877f2]">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+        <path
+          d="M80 299.3V512H196V299.3h86.5l18-97.8H196V166.9c0-51.7 20.3-71.5 72.7-71.5c16.3 0 29.4 .4 37 1.2V7.9C291.4 4 256.4 0 236.2 0C129.3 0 80 50.5 80 159.4v42.1H14v97.8H80z" />
+      </svg>
+    </a>
+  </li>
+  <li>
+    <a href="https://example.com/link3" className="[&>svg]:h-7 [&>svg]:w-7 [&>svg]:fill-[#ff0000]">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+        <path
+          d="M549.7 124.1c-6.3-23.7-24.8-42.3-48.3-48.6C458.8 64 288 64 288 64S117.2 64 74.6 75.5c-23.5 6.3-42 24.9-48.3 48.6-11.4 42.9-11.4 132.3-11.4 132.3s0 89.4 11.4 132.3c6.3 23.7 24.8 41.5 48.3 47.8C117.2 448 288 448 288 448s170.8 0 213.4-11.5c23.5-6.3 42-24.2 48.3-47.8 11.4-42.9 11.4-132.3 11.4-132.3s0-89.4-11.4-132.3zm-317.5 213.5V175.2l142.7 81.2-142.7 81.2z" />
+      </svg>
+    </a>
+  </li>
+
+  <li>
+    <a href="https://example.com/link5" className="[&>svg]:h-7 [&>svg]:w-7 [&>svg]:fill-[#c13584]">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+        <path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z" />
+      </svg>
+    </a>
+  </li>
+  <li>
+  <a href="https://wa.me/1234567890" className="[&>svg]:h-7 [&>svg]:w-7 [&>svg]:fill-[#128c7e]">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 448 512">
+      <path d="M380.9 97.1C339 55.1 283.2 32 223.9 32c-122.4 0-222 99.6-222 222 0 39.1 10.2 77.3 29.6 111L0 480l117.7-30.9c32.4 17.7 68.9 27 106.1 27h.1c122.3 0 224.1-99.6 224.1-222 0-59.3-25.2-115-67.1-157zm-157 341.6c-33.2 0-65.7-8.9-94-25.7l-6.7-4-69.8 18.3L72 359.2l-4.4-7c-18.5-29.4-28.2-63.3-28.2-98.2 0-101.7 82.8-184.5 184.6-184.5 49.3 0 95.6 19.2 130.4 54.1 34.8 34.9 56.2 81.2 56.1 130.5 0 101.8-84.9 184.6-186.6 184.6zm101.2-138.2c-5.5-2.8-32.8-16.2-37.9-18-5.1-1.9-8.8-2.8-12.5 2.8-3.7 5.6-14.3 18-17.6 21.8-3.2 3.7-6.5 4.2-12 1.4-32.6-16.3-54-29.1-75.5-66-5.7-9.8 5.7-9.1 16.3-30.3 1.8-3.7 .9-6.9-.5-9.7-1.4-2.8-12.5-30.1-17.1-41.2-4.5-10.8-9.1-9.3-12.5-9.5-3.2-.2-6.9-.2-10.6-.2-3.7 0-9.7 1.4-14.8 6.9-5.1 5.6-19.4 19-19.4 46.3 0 27.3 19.9 53.7 22.6 57.4 2.8 3.7 39.1 59.7 94.8 83.8 35.2 15.2 49 16.5 66.6 13.9 10.7-1.6 32.8-13.4 37.4-26.4 4.6-13 4.6-24.1 3.2-26.4-1.3-2.5-5-3.9-10.5-6.6z" />
+    </svg>
+  </a>
+</li>
+</ul>
+
             </div>
           </div>
         </div>
